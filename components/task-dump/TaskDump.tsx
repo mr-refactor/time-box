8e8b56bec8bb04ba0a30253fa814c01dd53e6a47
Tr1ckC0 <trick.co@yahoo.com>
2022-11-11 09:39:10 -0600

    added basic submit handler to form

--- components/task-dump/TaskDump.tsx
+++ components/task-dump/TaskDump.tsx
@@ -1,10 +1,22 @@
+import { FormEvent } from "react";
 import styles from "../../styles/TaskDump.module.css";
 
+const useForm = () => {
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        console.log('form submitted')
+    }
+
+    return handleSubmit;
+}
+
 export const TaskDump = () => {
+  const handleSubmit = useForm();
+
   return (
     <section className={styles.container}>
       <h2>Task Dump</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="tasks">Today&apos;s Tasks</label>
         <input id="tasks" name="tasks" placeholder="One task for today."></input>
         <button type="submit">Add</button>
