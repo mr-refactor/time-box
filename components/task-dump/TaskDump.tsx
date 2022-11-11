2c633dd0580e155b53fc395378b67032d8af33ba
Tr1ckC0 <trick.co@yahoo.com>
2022-11-11 09:29:05 -0600

    added form

--- components/task-dump/TaskDump.tsx
+++ components/task-dump/TaskDump.tsx
@@ -3,9 +3,13 @@ import styles from "../../styles/TaskDump.module.css";
 export const TaskDump = () => {
   return (
     <section className={styles.container}>
-        <h2>Task Dump</h2>
-        <div className={styles.box}>
-        </div>
+      <h2>Task Dump</h2>
+      <form>
+        <label htmlFor="tasks">Today&apos;s Tasks</label>
+        <input id="tasks" name="tasks" placeholder="One task for today."></input>
+        <button type="submit">Add</button>
+      </form>
+      <div className={styles.box}></div>
     </section>
-  )
-}
+  );
+};
